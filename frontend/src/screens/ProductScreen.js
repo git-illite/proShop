8c916ccf4f894d46ace3df3060864fc8318d4b69
git-illite <71469298+git-illite@users.noreply.github.com>
2023-12-27 18:58:32 -0500

    set up axios to work for homeScreen and productScreen (getting backend data)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link,useParams} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = () => {
     const { id } = useParams();
-    const product = products.find((p)=> p._id === id)
+    const [product, setProduct] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async ()=>{
+      const {data} = await axios.get(`/api/products/${id}`)
+      setProduct(data); 
+    }
+      fetchProducts();
+   
+  },[id]);
     
   return (
     <>
