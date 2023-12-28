08e7cf4ca30beebfab432991464d64871c4b4c2f
git-illite <71469298+git-illite@users.noreply.github.com>
2023-12-27 19:12:37 -0500

    installed dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv')
 
+dotenv.config();
 const app = express();
 const products = require('./data/products')
 
@@ -16,5 +18,5 @@ app.get('/api/products/:id',(req,res) =>{
     res.json(product)
 })
 
-
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
