632be128795dcb6025cfbaa9d25367dec852841b
git-illite <71469298+git-illite@users.noreply.github.com>
2024-01-07 14:47:19 -0500

    created and set up payment screen

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { FormContainer } from "../components/FormContainer.js";
+import { savePaymentMethod } from "../actions/cartActions.js";
+import CheckoutSteps from "../components/CheckoutSteps.js";
+
+const PaymentScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  if (!shippingAddress) {
+    navigate("/shipping");
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //console.log("submit handler called in shipping screen");
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="py-1">
+          <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+
+            {/* <Form.Check
+              type="radio"
+              label="Stripe"
+              id="Stripe"
+              name="paymentMethod"
+              value="Stripe"
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check> */}
+          </Col>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
