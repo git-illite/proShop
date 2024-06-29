76bd9846811c1d63b312243ca16bf2f5178b1f82
git-illite <71469298+git-illite@users.noreply.github.com>
2024-06-28 21:39:07 -0400

    added backend functionality to product reviews

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,10 +6,12 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { adminCheck, protect } from "../middleware/authMiddleWare.js";
 
 router.route("/").get(getProducts).post(protect, adminCheck, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 router
   .route("/:id")
